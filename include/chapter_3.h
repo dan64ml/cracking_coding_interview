73f019bc8360e4d355334cd02db76f4ea1d72600
dan <dan64ml@mail.ru>
2021-02-21 12:12:00 +0300

    Add ch3 problem4

--- include/chapter_3.h
+++ include/chapter_3.h
@@ -4,5 +4,6 @@
 #include "../src/03_stacks_queues/problem_1.h"
 #include "../src/03_stacks_queues/problem_2.h"
 #include "../src/03_stacks_queues/problem_3.h"
+#include "../src/03_stacks_queues/problem_4.h"
 
 #endif // CHAPTER_3_H_
---
+++ test/03_stacks_queues/test_problem_4.cpp
@@ -0,0 +1,43 @@
+#include "chapter_3.h"
+
+#include "gtest/gtest.h"
+
+#include <vector>
+
+using namespace std;
+
+TEST(Ch3Problem4, Test1) {
+    MyQueue<int> q;
+    EXPECT_THROW({
+        q.remove();
+    }, runtime_error);
+    EXPECT_THROW({
+        q.peek();
+    }, runtime_error);
+}
+
+TEST(Ch3Problem4, Test2) {
+    MyQueue<int> q;
+    vector vec {1, 2, 3, 4 ,5, 6, 7, 8, 9, 10};
+
+    for (auto e : vec) {
+        q.add(e);
+    }
+    for (auto e : vec) {
+        EXPECT_EQ(q.peek(), e);
+        EXPECT_EQ(q.remove(), e);
+    }
+}
+
+TEST(Ch3Problem4, Test3) {
+    MyQueue<int> q;
+
+    q.add(1);
+    q.add(2);
+    q.add(3);
+    EXPECT_EQ(q.remove(), 1);
+    EXPECT_EQ(q.remove(), 2);
+    q.add(4);
+    EXPECT_EQ(q.remove(), 3);
+    EXPECT_EQ(q.remove(), 4);
+}
